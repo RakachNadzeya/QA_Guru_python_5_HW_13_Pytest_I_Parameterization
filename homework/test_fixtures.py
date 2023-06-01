77a8d830aace73a325ab7c0864618f814f6e4a45
Nadzeya Rakach <nadezda.rakach@gmail.com>
2023-06-01 19:39:21 +0200

    Home task has been done

--- homework/test_fixtures.py
+++ homework/test_fixtures.py
@@ -1,11 +1,34 @@
 """
 Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
 """
+import pytest
+from selene import browser
 
 
-def test_github_desktop():
-    pass
+@pytest.fixture(params=[(1980, 1080), (1280, 1024)])
+def browser_size_desktop(request):
+    size = request.param
+    browser.open()
+    browser.driver.set_window_size(size[0], size[1])
+    yield browser
+    browser.quit()
 
 
-def test_github_mobile():
-    pass
+@pytest.fixture(params=[(360, 640), (390, 844)])
+def browser_size_mobile(request):
+    size = request.param
+    browser.open()
+    browser.driver.set_window_size(size[0], size[1])
+    yield browser
+    browser.quit()
+
+
+def test_github_desktop(browser_size_desktop):
+    browser.open("https://github.com/")
+    browser.element(".HeaderMenu-link--sign-up").click()
+
+
+def test_github_mobile(browser_size_mobile):
+    browser.open("https://github.com/")
+    browser.element(".Button--link").click()
+    browser.element(".HeaderMenu-link--sign-in").click()
--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -2,16 +2,26 @@
 Переопределите параметр с помощью indirect параметризации на уровне теста
 """
 import pytest
+from selene import browser
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=[(1980, 1080), (1280, 1024), (360, 640), (390, 844)])
+def browser_size(request):
+    size = request.param
+    browser.open()
+    browser.driver.set_window_size(size[0], size[1])
+    yield browser
+    browser.quit()
 
 
-def test_github_desktop(browser):
-    pass
+@pytest.mark.parametrize('browser_size', [(1980, 1080), (1280, 1024)], indirect=True)
+def test_github_desktop(browser_size):
+    browser.open('https://github.com/')
+    browser.element('.HeaderMenu-link--sign-up').click()
 
 
-def test_github_mobile(browser):
-    pass
+@pytest.mark.parametrize('browser_size', [(360, 640)], indirect=True)
+def test_github_mobile(browser_size):
+    browser.open('https://github.com/')
+    browser.element('.Button--link').click()
+    browser.element('.HeaderMenu-link--sign-in').click()
